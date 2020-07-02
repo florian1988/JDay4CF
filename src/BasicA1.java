8a6c7019089967e3f33b6d196b620b77344f567f
florian <flo.moerzinger@gmx.at>
2020-07-02 10:31:04 +0200

    basic

--- src/BasicA1.java
+++ src/BasicA1.java
@@ -1,5 +1,15 @@
 public class BasicA1 {
-    public static void main(String[] args) {
 
+    public static void A1(){
+        for (int i = 0; i < 100; i++) {
+            int amount = i + 1;
+
+            System.out.println(amount);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        A1();
     }
 }
---
+++ src/BasicA2.java
@@ -0,0 +1,13 @@
+public class BasicA2 {
+
+    public static void A2(){
+        for (int i = 2; i < 101; i = i + 2) {
+            System.out.println(i);
+        }
+    }
+
+
+    public static void main(String[] args) {
+        A2();
+    }
+}
